c926985f1dcef82a243f8554cfc6e0e0018b0840
Dana Simmons <dana@simmons-audio.com>
2019-02-03 09:38:46 -0500

    Done.

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,15 @@
 class Vehicle
+  attr_accessor :wheel_size, :wheel_number
+  def initialize(wheel_zie, wheel_number)
+    @wheel_zie = wheel_zie
+    @wheel_number = wheel_number
+  end
 
+  def go
+    "vrrrrrrrooom!"
+  end
+
+  def fill_up_tank
+    "filling up!"
+  end
 end
